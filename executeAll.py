e0422905d4071416802577242d1f1b04bfaeb019
MattiaBiancini <m.biancini1@campus.unimib.it>
2024-11-25 20:12:43 +0100

    Bugfix

--- executeAll.py
+++ executeAll.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import pandas as pd
 
+
 def initialize_csv(results_file):
     if not os.path.exists(results_file):
         # Create an empty DataFrame with the required columns
@@ -9,6 +10,7 @@ def initialize_csv(results_file):
         # Save it to a CSV file
         df.to_csv(results_file, index=False)
         print(f"Created {results_file} with header.")
+
 
 # Define the base directory for your project
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -19,12 +21,15 @@ initialize_csv(results_file)
 # Loop through subdirectories to find and execute .sh files
 for root, dirs, files in os.walk(script_dir):
     for file in files:
-        if file.endswith(".sh"):  # Look for .sh files
+        if file.endswith(".sh"):
             sh_file_path = os.path.join(root, file)
-            print(f"Executing: {sh_file_path}")
+            script_dir = os.path.dirname(sh_file_path)
+            while "script" in script_dir:
+                script_dir = os.path.dirname(script_dir)# Get the directory of the script
+            print(f"Executing: {os.path.normpath(sh_file_path)} from {script_dir}")
 
             try:
-                subprocess.run(["bash", sh_file_path], check=True)
+                # Run the shell script with its directory as the working directory
+                subprocess.run(["bash", sh_file_path.replace("C:", "/mnt/c").replace("\\", "/")], cwd=script_dir, check=True)
             except subprocess.CalledProcessError as e:
                 print(f"Error while executing {sh_file_path}: {e}")
-
